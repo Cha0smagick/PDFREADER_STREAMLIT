b9ac09231d92131b741f72a4b254e3c9c71eaa1c
Cha0smagick <alekos20@gmail.com>
2023-12-20 15:19:54 -0500

    Update app_gemini.py

--- app_gemini.py
+++ app_gemini.py
@@ -8,39 +8,54 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import spacy
 import subprocess
+from langdetect import detect
+from deep_translator import GoogleTranslator
+
+# Diccionario de modelos de spaCy por idioma
+SPACY_MODELS = {
+    "en": "en_core_web_sm",
+    "es": "es_core_news_sm",
+    "de": "de_core_news_sm",
+    "fr": "fr_core_news_sm",
+    "it": "it_core_news_sm"
+}
 
 # Función para descargar el modelo de spaCy si aún no está instalado
-def download_spacy_model(model_name="en_core_web_sm"):
+def download_spacy_model(language_code):
+    model_name = SPACY_MODELS.get(language_code, "en_core_web_sm")
     try:
-        # Intenta cargar el modelo para ver si ya está instalado
         spacy.load(model_name)
         print(f"Modelo de spaCy '{model_name}' ya está instalado.")
     except OSError:
-        # Si no está instalado, lo descarga e instala
         print(f"Descargando e instalando el modelo '{model_name}'...")
         subprocess.run(["python", "-m", "spacy", "download", model_name])
-
-# Llama a la función para asegurarte de que el modelo está instalado
-download_spacy_model()
-
-# Cargar el modelo de spaCy
-nlp = spacy.load("en_core_web_sm")
+        spacy.load(model_name)  # Cargar el modelo después de instalarlo
 
 # Configuración de Google Gemini
-GOOGLE_API_KEY = 'your_google_api_key'
+GOOGLE_API_KEY = 'AIzaSyAkbU3CsZ-xmOhRF1XfdlVxasRtt9gdRMk'
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
+# Función para detectar el idioma de un texto
+def detect_language(text):
+    try:
+        return detect(text)
+    except:
+        return "en"  # Devolver inglés por defecto si la detección falla
+
+# Función para traducir texto
+def translate_text(text, target_lang):
+    translator = GoogleTranslator(source='auto', target=target_lang)
+    return translator.translate(text)
+
 # Función para enriquecer el texto con análisis sintáctico, semántico y estructural
-def enrich_text(text):
+def enrich_text(text, nlp):
     doc = nlp(text)
     enriched_sentences = []
     for sent in doc.sents:
-        # Encontrar entidades y dependencias
         entities = " ".join([ent.text for ent in sent.ents])
         deps = " ".join([token.dep_ for token in sent])
 
-        # Identificar posibles títulos o subtítulos (aquí usamos un enfoque simple)
         is_title = sent.text.isupper() or any(token.is_title for token in sent)
 
         enriched_sentence = f"{sent.text} {entities} {deps}"
@@ -50,23 +65,17 @@ def enrich_text(text):
         enriched_sentences.append(enriched_sentence)
     return " ".join(enriched_sentences)
 
-# Función mejorada para encontrar oraciones relevantes y manejar diferentes tipos de preguntas
-def find_relevant_information(question, text):
+# Función para encontrar oraciones relevantes y manejar diferentes tipos de preguntas
+def find_relevant_information(question, text, nlp):
     nltk.download('punkt')
-    enriched_text = enrich_text(text)
+    enriched_text = enrich_text(text, nlp)
     sentences = nltk.sent_tokenize(enriched_text)
-
-    if "resumen" in question.lower():
-        # Generar un resumen (aquí podríamos usar un enfoque más sofisticado)
-        return " ".join(sentences[:min(5, len(sentences))])
 
     vectorizer = TfidfVectorizer().fit([question] + sentences)
     question_vec = vectorizer.transform([question])
     sentences_vec = vectorizer.transform(sentences)
 
     similarities = cosine_similarity(question_vec, sentences_vec).flatten()
-
-    # Mejora del sistema de ponderación para diferentes tipos de preguntas
     sorted_indices = similarities.argsort()[::-1]
     relevant_indices = sorted_indices[:5]
 
@@ -80,11 +89,9 @@ def generate_response(query, context):
         response = model.generate_content(prompt)
         return response.text
     except ValueError as e:
-        # Verificar si el error se debe a un problema con el prompt
         if "none were returned" in str(e):
             return "Lo siento, no puedo procesar esa pregunta debido a las restricciones de políticas de Google Gemini."
         else:
-            # Si es otro tipo de ValueError, se re-lanza la excepción
             raise e
 
 # Interfaz de usuario de Streamlit
@@ -94,9 +101,16 @@ query = st.text_input("Introduce tu pregunta")
 
 if st.button("Enviar") and pdf_file is not None and query:
     pdf_content = BytesIO(pdf_file.read())
+    text = ""
     with fitz.open(stream=pdf_content, filetype="pdf") as doc:
         text = " ".join(page.get_text() for page in doc)
 
-    context = find_relevant_information(query, text)
+    detected_lang = detect_language(text)
+    download_spacy_model(detected_lang)
+    nlp = spacy.load(SPACY_MODELS.get(detected_lang, "en_core_web_sm"))
+
+    translated_query = translate_text(query, detected_lang)
+    context = find_relevant_information(translated_query, text, nlp)
+
     answer = generate_response(query, context)
     st.write(answer)
