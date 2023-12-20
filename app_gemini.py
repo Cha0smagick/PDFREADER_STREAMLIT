c2c330bdbe04ead3d1fc327aa983a98a43c2c6ed
Cha0smagick <alekos20@gmail.com>
2023-12-20 13:18:32 -0500

    Update app_gemini.py

--- app_gemini.py
+++ app_gemini.py
@@ -27,40 +27,48 @@ download_spacy_model()
 nlp = spacy.load("en_core_web_sm")
 
 # Configuración de Google Gemini
-GOOGLE_API_KEY = 'google_api_key'
+GOOGLE_API_KEY = 'your_google_api_key'
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
-# Función para enriquecer el texto con análisis sintáctico y semántico
+# Función para enriquecer el texto con análisis sintáctico, semántico y estructural
 def enrich_text(text):
     doc = nlp(text)
     enriched_sentences = []
     for sent in doc.sents:
+        # Encontrar entidades y dependencias
         entities = " ".join([ent.text for ent in sent.ents])
         deps = " ".join([token.dep_ for token in sent])
+
+        # Identificar posibles títulos o subtítulos (aquí usamos un enfoque simple)
+        is_title = sent.text.isupper() or any(token.is_title for token in sent)
+
         enriched_sentence = f"{sent.text} {entities} {deps}"
+        if is_title:
+            enriched_sentence += " TITLE_OR_SUBTITLE"
+
         enriched_sentences.append(enriched_sentence)
     return " ".join(enriched_sentences)
 
-# Función mejorada para encontrar oraciones relevantes
-def find_relevant_sentences(question, text, num_sentences=5):
+# Función mejorada para encontrar oraciones relevantes y manejar diferentes tipos de preguntas
+def find_relevant_information(question, text):
     nltk.download('punkt')
     enriched_text = enrich_text(text)
     sentences = nltk.sent_tokenize(enriched_text)
+
+    if "resumen" in question.lower():
+        # Generar un resumen (aquí podríamos usar un enfoque más sofisticado)
+        return " ".join(sentences[:min(5, len(sentences))])
+
     vectorizer = TfidfVectorizer().fit([question] + sentences)
     question_vec = vectorizer.transform([question])
     sentences_vec = vectorizer.transform(sentences)
 
     similarities = cosine_similarity(question_vec, sentences_vec).flatten()
 
-    # Mejora del sistema de ponderación
+    # Mejora del sistema de ponderación para diferentes tipos de preguntas
     sorted_indices = similarities.argsort()[::-1]
-    relevant_indices = []
-    for idx in sorted_indices:
-        if len(relevant_indices) < num_sentences:
-            relevant_indices.append(idx)
-        else:
-            break
+    relevant_indices = sorted_indices[:5]
 
     relevant_sentences = [sentences[i] for i in relevant_indices]
     return " ".join(relevant_sentences)
@@ -81,6 +89,6 @@ if st.button("Enviar") and pdf_file is not None and query:
     with fitz.open(stream=pdf_content, filetype="pdf") as doc:
         text = " ".join(page.get_text() for page in doc)
 
-    context = find_relevant_sentences(query, text)
+    context = find_relevant_information(query, text)
     answer = generate_response(query, context)
     st.write(answer)
